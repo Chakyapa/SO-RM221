3cc0d5605736259a4d3cfe2d07b054ef5b9a6ff0
Aesthetic <andreiclima5@gmail.com>
2025-02-18 12:21:51 +0200

    interface and functionality changed

--- src/main/java/org/example/solab1/Application.java
+++ src/main/java/org/example/solab1/Application.java
@@ -7,10 +7,12 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
+    private Controller controller;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 547, 498);
+        controller = fxmlLoader.getController();
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
@@ -18,5 +20,12 @@ public class Application extends javafx.application.Application {
 
     public static void main(String[] args) {
         launch();
+    }
+
+    @Override
+    public void stop() {
+        if (controller != null) {
+            controller.stopTasks();
+        }
     }
 }
--- src/main/java/org/example/solab1/Controller.java
+++ src/main/java/org/example/solab1/Controller.java
@@ -2,12 +2,14 @@ package org.example.solab1;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import org.example.solab1.remote.MeteoAPI;
 
+import java.net.URL;
 import java.util.*;
 
-public class Controller {
+public class Controller implements Initializable {
     @FXML
     private Label temperature;
     @FXML
@@ -16,23 +18,60 @@ public class Controller {
     private Label precipitation;
     @FXML
     private Label water;
-
     @FXML
-    protected void onHelloButtonClick() {
+    private TextField waterInput;
+    @FXML
+    private Spinner<Integer> hourSpinner;
+    @FXML
+    private Spinner<Integer> minuteSpinner;
+
+    private Timer waterTimer;
+    private Timer meteoTimer;
+    private Timer notificationTimer;
+
+    private int mlTotal = 0;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
         startMeteoUpdates();
-        startWaterSchedule();
+        hourSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 23, 7));
+        minuteSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 59, 0));
+        water.setText(mlTotal + " ml");
     }
 
-    private void startMeteoUpdates() {
-        Timer meteoTimer = new Timer();
+    public void onAddWater() {
+        try {
+            int mlDrink = Integer.parseInt(waterInput.getText());
+            int ml = Integer.parseInt(water.getText().split(" ")[0]);
+
+            mlTotal = ml + mlDrink;
+
+            water.setText(mlTotal + " ml");
+        } catch (NumberFormatException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Please enter a valid water value");
+            alert.showAndWait();
+        }
+    }
+
+    public void onSetReminder() {
+        int hour = hourSpinner.getValue();
+        int minute = minuteSpinner.getValue();
+        startWaterSchedule(hour, minute);
+    }
+
+    protected void startMeteoUpdates() {
+        meteoTimer = new Timer();
         meteoTimer.scheduleAtFixedRate(new Meteo(this), 0, 2000);
     }
 
-    private void startWaterSchedule() {
-        Timer waterTimer = new Timer();
+    private void startWaterSchedule(int hour, int minute) {
+        waterTimer = new Timer();
         Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, 13);
-        calendar.set(Calendar.MINUTE, 17);
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, minute);
         calendar.set(Calendar.SECOND, 0);
 
         Date date = calendar.getTime();
@@ -42,21 +81,37 @@ public class Controller {
             date = calendar.getTime();
         }
 
-        waterTimer.schedule(new WaterMessage("It's time to drink water!!!",this), date);
+        waterTimer.schedule(new WaterMessage("It's time to drink water!!!"), date);
     }
 
     public void updateTemperature(double temperature, double humidity, double precipitation) {
         Platform.runLater(() -> {
-            this.temperature.setText("Temperatura: " + temperature + "°C");
-            this.humidity.setText("Humidade: " + humidity + "%");
-            this.precipitation.setText("Precipitation: " + precipitation + "mm");
+            this.temperature.setText(temperature + "°C");
+            this.humidity.setText(humidity + "%");
+            this.precipitation.setText(precipitation + "mm");
 
         });
-        System.out.println("Temperatura: " + temperature);
     }
 
-    public void setWaterSchedule(String waterTime) {
-        Platform.runLater(() -> water.setText(waterTime));
+    public void stopTasks() {
+        stopMeteoUpdates();
+        stopWaterSchedule();
+    }
+
+    private void stopMeteoUpdates() {
+        if (meteoTimer != null) {
+            meteoTimer.cancel();
+            meteoTimer.purge();
+            meteoTimer = null;
+        }
+    }
+
+    private void stopWaterSchedule() {
+        if (waterTimer != null) {
+            waterTimer.cancel();
+            waterTimer.purge();
+            waterTimer = null;
+        }
     }
 }
 
@@ -75,30 +130,37 @@ class Meteo extends TimerTask {
 
             controller.updateTemperature(meteoInfo.get("temperature"), meteoInfo.get("humidity"), meteoInfo.get("precipitation"));
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("Meteo API Error");
         }
     }
 }
 
 class WaterMessage extends TimerTask {
     private final String message;
-    private final Controller controller;
 
-    public WaterMessage(String message, Controller controller) {
+    public WaterMessage(String message) {
         this.message = message;
-        this.controller = controller;
+    }
+
+    public void showNotification(String message) {
+        Platform.runLater(() -> {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Water Message");
+            alert.setHeaderText(null);
+            alert.setContentText(message);
+            alert.show();
+
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    Platform.runLater(alert::close);
+                }
+            }, 30 * 1000);
+        });
     }
 
     @Override
     public void run() {
-
-        controller.setWaterSchedule(message);
-
-        new Timer().schedule(new TimerTask() {
-            @Override
-            public void run() {
-                controller.setWaterSchedule("");
-            }
-        }, 60 * 1000);
+        showNotification(message);
     }
 }
