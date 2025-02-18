d21d60acdd76f0ca3797c1dfbaf826f8e77230dc
Deviant <alexei.meisov@isa.utm.md>
2025-02-18 14:58:38 +0200

    периодический таймер, интерфейс

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.time.LocalTime;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Main {
     public static void main(String[] args) {
+        boolean startCount = false;
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("лабораторная");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -16,6 +18,11 @@ public class Main {
             Balls ballsPanel = new Balls();
             frame.add(ballsPanel);
 
+
+
+
+
+
             frame.addKeyListener(new KeyAdapter() {
                 @Override
                 public void keyPressed(KeyEvent e) {
@@ -23,26 +30,29 @@ public class Main {
                         case KeyEvent.VK_LEFT:
                             if (ballsPanel.getSelectedBall() == 0) {
                                 ballsPanel.incrementScore(5);
-                                ballsPanel.selectBall(4);
-                            }
+
+                            }ballsPanel.selectBall(4);
                             break;
                         case KeyEvent.VK_RIGHT:
                             if (ballsPanel.getSelectedBall() == 1) {
                                 ballsPanel.incrementScore(5);
-                                ballsPanel.selectBall(4);
-                            }
+
+                            }ballsPanel.selectBall(4);
                             break;
                         case KeyEvent.VK_UP:
                             if (ballsPanel.getSelectedBall() == 2) {
                                 ballsPanel.incrementScore(5);
-                                ballsPanel.selectBall(4);
-                            }
+
+                            }ballsPanel.selectBall(4);
                             break;
                         case KeyEvent.VK_DOWN:
                             if (ballsPanel.getSelectedBall() == 3) {
                                 ballsPanel.incrementScore(5);
-                                ballsPanel.selectBall(4);
+
                             }
+                            ballsPanel.selectBall(4);
+
+
                             break;
                         case KeyEvent.VK_SPACE:
                             ballsPanel.resetScore();
@@ -68,7 +78,10 @@ public class Main {
 class Balls extends JPanel {
     private int selectedBall = -1;
     private int score = 0;
-
+    private boolean gameOver = false;
+    public Balls(){
+        startTimer();
+    }
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -81,6 +94,7 @@ class Balls extends JPanel {
         int cordY = getHeight() / 2;
         int diam = 50;
         int razn = 60;
+
 
         int[][] positions = {
                 {cordX - razn, cordY},    // Лево
@@ -95,7 +109,14 @@ class Balls extends JPanel {
             } else {
                 g.setColor(Color.BLUE);
             }
-            g.fillOval(positions[i][0] - diam / 2, positions[i][1] - diam / 2, diam, diam);
+            if(!gameOver) {
+                g.fillOval(positions[i][0] - diam / 2, positions[i][1] - diam / 2, diam, diam);
+            } else{
+                g.setColor(Color.BLACK);
+                g.setFont(new Font("Arial", Font.BOLD, 36));
+                g.drawString("Время вышло" , 20, getHeight()/2);
+            }
+
         }
     }
 
@@ -125,4 +146,20 @@ class Balls extends JPanel {
         score = 0;
         repaint();
     }
+    private void startTimer() {
+        LocalTime startTime = LocalTime.now(); // Запоминаем время старта
+        System.out.println("Таймер запущен в: " + startTime); // Вывод в консоль
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                LocalTime endTime = LocalTime.now(); // Фиксируем время окончания
+                System.out.println("Игра окончена в: " + endTime); // Выводим время окончания
+                gameOver=true;
+                repaint();
+            }
+        }, 5 * 60 * 1000); // 5 минут в миллисекундах
+    }
+
 }
