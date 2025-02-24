44a6cf204dee27b3e7ac93e283e1fb54b4af55e4
inna <innakashkaval12@gmail.com>
2025-02-25 01:13:44 +0200

    add sound

--- Lab_1/src/ProductivityTimerGUI.java
+++ Lab_1/src/ProductivityTimerGUI.java
@@ -10,7 +10,7 @@ import java.io.File;
 public class ProductivityTimerGUI extends JPanel{
 
     // путь к звуковой дорожке, которая звучит при окончании таймера
-    static final String END_SOUND_PATH = "/Users/stepan_iularji/IdeaProjects/untitled/src/sound.wav";
+    static final String END_SOUND_PATH = "C:/Users/innak/Desktop/tum/3curs/2sem/SO/SO-RM221/sound.wav";
 
     // размеры окна приложения
     static final int WINDOW_WIDTH = 800;
