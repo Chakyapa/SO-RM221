4ad982042212f208cfcf7583da9d739d5c373acb
Sermistr <sermistr21@gmail.com>
2025-03-18 23:47:03 +0200

    Serghei Lazarev's part is done. I have just written Reader's and Library's part

--- Laba2_SO_Project/src/Library.java
+++ Laba2_SO_Project/src/Library.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-class Reader extends Thread {
+class Writer extends Thread {
 
 }
 
@@ -18,19 +18,19 @@ class Library {
         this.writers = new Writer[writers];
         this.readers = new Reader[readers];
         Library.books = books;
-        for (int i = 0; i < writers; i++) {
+        /*for (int i = 0; i < writers; i++) {
             this.writers[i] = new Writer("Writer " + (i+1));
-        }
-        /*for (int i = 0; i < readers; i++) {
-            this.readers[i] = new Reader("Reader " + (i+1));
         }*/
+        for (int i = 0; i < readers; i++) {
+            this.readers[i] = new Reader("Reader " + (i+1));
+        }
     }
     public void start() {
-        for (Writer writer : this.writers) {
+        /*for (Writer writer : this.writers) {
             writer.start();
-        }
-        /*for (Reader reader : this.readers) {
-            reader.start();
         }*/
+        for (Reader reader : this.readers) {
+            reader.start();
+        }
     }
 }
