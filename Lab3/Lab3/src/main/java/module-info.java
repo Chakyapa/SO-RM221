8fe1d652b1aeb5ae233ccea0937c5c4b1b19f302
Maslova Xenia <ksyushamaslova17@gmail.com>
2025-04-02 16:22:09 +0300

    Попытка2

--- Lab3/Lab3/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.lab3 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.lab3 to javafx.fxml;
-    exports com.example.lab3;
-}
